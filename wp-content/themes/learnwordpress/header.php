34db720b821bd75d2290a2eddf11d6dbacf8aded
Rami Mustaklem <ramimustaklem@gmail.com>
2018-12-08 18:08:44 +0200

    code cleaning

--- wp-content/themes/learnwordpress/header.php
+++ wp-content/themes/learnwordpress/header.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title><?php echo get_bloginfo( 'name' ); ?></title>
+    <title><?php echo get_bloginfo('name'); ?></title>
 
     <!-- Bootstrap core CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
@@ -22,7 +22,7 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <?php wp_head();?>
+    <?php wp_head(); ?>
 </head>
 
 <body>
@@ -31,7 +31,7 @@
     <div class="container">
         <nav class="blog-nav">
             <a class="blog-nav-item active" href="#">Home</a>
-            <?php wp_list_pages( '&title_li=' ); ?>
+            <?php wp_list_pages('&title_li='); ?>
         </nav>
     </div>
 </div>
@@ -39,6 +39,8 @@
 <div class="container">
 
     <div class="blog-header">
-        <h1 class="blog-title"><a href="<?php echo get_bloginfo( 'wpurl' );?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
-        <p class="lead blog-description"><?php echo get_bloginfo( 'description' ); ?></p>
+        <h1 class="blog-title">
+            <a href="<?php echo get_bloginfo('wpurl'); ?>"><?php echo get_bloginfo('name'); ?></a>
+        </h1>
+        <p class="lead blog-description"><?php echo get_bloginfo('description'); ?></p>
     </div>
